b087b6be598bb66c6eb507a4f94223b469688f0d
Dr0zax <geodashfan4@gmail.com>
2025-10-04 15:23:48 -0600

    Implement CRUD operations for contacts in the controller and update routes

--- controllers/contacts.js
+++ controllers/contacts.js
@@ -31,4 +31,46 @@ const getSingle = async (req, res) => {
     }
 };
 
-module.exports = { getAll, getSingle };
+const create = async (req, res) => {
+    try {
+        const db = mongodb.getDb();
+        const newContact = {
+            firstName: req.body.firstName,
+            lastName: req.body.lastName,
+            email: req.body.email,
+            favoriteColor: req.body.favoriteColor,
+            birthday: req.body.birthday
+        };
+        const result = await db.collection("contacts").insertOne(newContact);
+        res.status(201).json(result);
+    } catch (err) {
+        res.status(500).json({ message: err.message });
+    }
+};
+
+const update = async (req, res) => {
+    try {
+    const db = mongodb.getDb();
+    const contactId = new ObjectId(req.params.id);
+    const updatedContact = {
+        firstName: req.body.firstName,
+        lastName: req.body.lastName,
+        email: req.body.email,
+        favoriteColor: req.body.favoriteColor,
+        birthday: req.body.birthday
+    };
+    const result = await db.collection("contacts").replaceOne({ _id: contactId }, updatedContact);
+    res.status(204).send();
+    } catch (err) {
+        res.status(500).json({ message: err.message });
+    }
+};
+
+const remove = async (req, res) => {
+    const db = mongodb.getDb();
+    const contactId = new ObjectId(req.params.id);
+    const result = await db.collection("contacts").deleteOne({ _id: contactId }, true);
+    res.status(204).json(result);
+};
+
+module.exports = { getAll, getSingle, create, update, remove };
